0ff433e58b32d8ef7a16f05f3c6c9a0c59993739
Blade Boles <bladerboles@gmail.com>
2022-12-09 14:17:18 -0700

    feat: add use-timer-hook library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useTimer, useStopwatch } from 'vue-timer-hook'
+
+const time = new Date()
+time.setSeconds(time.getSeconds() + 600) // 10 minutes timer
+const timer = useTimer(time)
+const restartFive = () => {
+  // Restarts to 5 minutes timer
+  const time = new Date()
+  time.setSeconds(time.getSeconds() + 300)
+  timer.restart(time)
+}
+
+const autoStart = true
+const stopwatch = useStopwatch(autoStart)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <h1>vue-timer-hook</h1>
+      <p>Timer Demo</p>
+      <div>
+        <span>{{ timer.days }}</span
+        >:<span>{{ timer.hours }}</span
+        >:<span>{{ timer.minutes }}</span
+        >:<span>{{ timer.seconds }}</span>
+      </div>
+      <p>{{ timer.isRunning ? 'Running' : 'Not running' }}</p>
+      <button @click="timer.start()">Start</button>
+      <button @click="timer.pause()">Pause</button>
+      <button @click="timer.resume()">Resume</button>
+      <button @click="restartFive()">Restart</button>
+    </div>
+    <div>
+      <h1>vue-timer-hook</h1>
+      <p>Stopwatch Demo</p>
+      <div>
+        <span>{{ stopwatch.days }}</span
+        >:<span>{{ stopwatch.hours }}</span
+        >:<span>{{ stopwatch.minutes }}</span
+        >:<span>{{ stopwatch.seconds }}</span>
+      </div>
+      <p>{{ stopwatch.isRunning ? 'Running' : 'Not running' }}</p>
+      <button @click="stopwatch.start()">Start</button>
+      <button @click="stopwatch.pause()">Pause</button>
+      <button @click="stopwatch.reset()">Reset</button>
+    </div>
   </main>
 </template>
