2d5cfd710f793396c614e7f5fe75d572598d8272
Blade Boles <bladerboles@gmail.com>
2023-07-23 11:26:42 -0600

    fix: weird import stuff with shim file, feat: get sign up and login forms 'working'

--- web/src/views/LoginView.vue
+++ web/src/views/LoginView.vue
@@ -1,55 +1,88 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import AuthServices from '../services/AuthServices'
 
 const loginChoice = ref('Log In')
+const signupPassword = ref('')
+const signupEmail = ref('')
+const loginPassword = ref('')
+const loginEmail = ref('')
+
+const signUp = async () => {
+  console.log('signing up...')
+  await AuthServices.signUserUp(signupEmail.value, signupPassword.value)
+}
+const logIn = async () => {
+  await AuthServices.logUserIn(loginEmail.value, loginPassword.value)
+}
 </script>
 
 <template>
-  <fieldset>
-    <legend class="login-view__select-action">Login or Signup</legend>
-    <div>
-      <input
-        type="radio"
-        id="login-choice"
-        name="choice"
-        value="Log In"
-        v-model="loginChoice"
-        class="login-view__radio-input"
-      />
-      <label class="login-view__radio-label" for="login-choice">Log In</label>
-    </div>
-    <div>
-      <input
-        type="radio"
-        id="signup-choice"
-        name="choice"
-        value="Sign Up"
-        v-model="loginChoice"
-        class="home-view__radio-input"
-      />
-      <label class="login-view__radio-label" for="signup-choice">Sign Up</label>
-    </div>
-  </fieldset>
-  <form class="login-view__signup-form">
-    <div class="login-view__signup-form-section">
-      <input type="email" id="signup-email" required />
-      <label for="signup-email"> Enter your email: </label>
-    </div>
-    <div class="login-view__signup-form-section">
-      <input type="password" id="signup-password" required />
-      <label for="signup-password"> Enter your password: </label>
-    </div>
-  </form>
-  <form class="login-view__login-form">
-    <div class="login-view__login-form-section">
-      <input type="email" id="login-email" required />
-      <label for="login-email"> Enter your email: </label>
-    </div>
-    <div class="login-view__login-form-section">
-      <input type="password" id="login-password" required />
-      <label for="login-password"> Enter your password: </label>
-    </div>
-  </form>
+  <div>
+    <fieldset>
+      <legend class="login-view__select-action">Login or Signup</legend>
+      <div>
+        <input
+          type="radio"
+          id="login-choice"
+          name="choice"
+          value="Log In"
+          v-model="loginChoice"
+          class="login-view__radio-input"
+        />
+        <label class="login-view__radio-label" for="login-choice">Log In</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="signup-choice"
+          name="choice"
+          value="Sign Up"
+          v-model="loginChoice"
+          class="home-view__radio-input"
+        />
+        <label class="login-view__radio-label" for="signup-choice"
+          >Sign Up</label
+        >
+      </div>
+    </fieldset>
+    <form
+      @submit.prevent="signUp"
+      class="login-view__signup-form"
+      v-if="loginChoice === 'Sign Up'"
+    >
+      <div class="login-view__signup-form-section">
+        <label for="signup-email"> Email: </label>
+        <input v-model="signupEmail" type="email" id="signup-email" required />
+      </div>
+      <div class="login-view__signup-form-section">
+        <label for="signup-password"> Password: </label>
+        <input
+          v-model="signupPassword"
+          type="password"
+          id="signup-password"
+          required
+        />
+      </div>
+      <button>Create Account</button>
+    </form>
+    <form @submit.prevent="logIn" v-else class="login-view__login-form">
+      <div class="login-view__login-form-section">
+        <label for="login-email"> Email: </label>
+        <input v-model="loginEmail" type="email" id="login-email" required />
+      </div>
+      <div class="login-view__login-form-section">
+        <label for="login-password"> Password: </label>
+        <input
+          v-model="loginPassword"
+          type="password"
+          id="login-password"
+          required
+        />
+      </div>
+      <button>Log In</button>
+    </form>
+  </div>
 </template>
 
 <style scoped></style>
