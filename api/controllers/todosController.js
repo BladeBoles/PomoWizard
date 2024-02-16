10fb01b1a05d4a9af2799caae45e2c33c43d9c5b
Blade Boles <bladerboles@gmail.com>
2024-02-15 18:02:19 -0700

    feat: move todos methods to controller file

---
+++ api/controllers/todosController.js
@@ -0,0 +1,48 @@
+const User = require('../models/User')
+const asyncHandler = require('express-async-handler')
+
+exports.create = asyncHandler(async (req, res) => {
+  const { email } = req.userData
+  const { title, description, completed, dateCompleted } = req.body
+  const newTodo = { title, description, completed, dateCompleted }
+  try {
+    const todoOwner = await User.findOne({
+      email
+    })
+    if (!todoOwner) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    todoOwner.todos.push(newTodo)
+
+    const updatedUser = await todoOwner.save()
+
+    res.json(updatedUser.todos)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'An error occurred' })
+  }
+})
+
+exports.updateAll = asyncHandler(async (req, res) => {
+  const { email } = req.userData
+  const { todos } = req.body
+  try {
+    const result = await User.findOneAndUpdate(
+      {
+        email
+      },
+      {
+        $set: { todos }
+      },
+      { new: true }
+    )
+    if (!result) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    result.json(result.todos)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'An error occurred' })
+  }
+})
--- api/routes/Todos.js
+++ api/routes/Todos.js
@@ -1,51 +1,10 @@
 const { Router } = require('express')
 const User = require('../models/User')
 const authMiddleware = require('../middlewares/auth')
+const todos_controller = require('../controllers/todosController')
 
 const router = Router()
 
-router.post('/todos/create', authMiddleware, async (req, res) => {
-  const { email } = req.userData
-  const { title, description, completed, dateCompleted } = req.body
-  const newTodo = { title, description, completed, dateCompleted }
-  try {
-    const todoOwner = await User.findOne({
-      email
-    })
-    if (!todoOwner) {
-      return res.status(404).json({ message: 'User not found' })
-    }
-
-    todoOwner.todos.push(newTodo)
-
-    const updatedUser = await todoOwner.save()
-
-    res.json(updatedUser.todos)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'An error occurred' })
-  }
-})
-router.post('/todos/updateAll', authMiddleware, async (req, res) => {
-  const { email } = req.userData
-  const { todos } = req.body
-  try {
-    const result = await User.findOneAndUpdate(
-      {
-        email
-      },
-      {
-        $set: { todos }
-      },
-      { new: true }
-    )
-    if (!result) {
-      return res.status(404).json({ message: 'User not found' })
-    }
-    result.json(result.todos)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'An error occurred' })
-  }
-})
+router.post('/todos/create', authMiddleware, todos_controller.create)
+router.post('/todos/updateAll', authMiddleware, todos_controller.updateAll)
 module.exports = router
