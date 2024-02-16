3c71b9346301f3a1202917b128b485d5ea0751e9
Blade Boles <bladerboles@gmail.com>
2024-02-15 18:45:54 -0700

    feat: add services method for get todos

--- web/src/services/TodoServices.ts
+++ web/src/services/TodoServices.ts
@@ -10,6 +10,15 @@ interface ResponseTodo extends Todo {
   _id: string
 }
 export default {
+  getTodos: async () => {
+    const response = await axios.get<ResponseTodo[]>(
+      `${import.meta.env.VITE_API_HOST}/api/todos`,
+      {
+        headers: { Authorization: `Bearer ${token}` }
+      }
+    )
+    return response
+  },
   createTodo: async (todo: Todo) => {
     const response = await axios.post<ResponseTodo[]>(
       `${import.meta.env.VITE_API_HOST}/api/todos/create`,
